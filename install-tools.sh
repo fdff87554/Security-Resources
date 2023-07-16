5938e5a6e60ea951c8572b85ce79d3a71904e83d
Crazyfire Lee <me@crazyfirelee.tw>
2023-07-16 14:20:26 +0800

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -13,7 +13,7 @@ sudo apt install curl wget python3 python3-pip vim git -y
 
 # -----------------------------
 
-# Install DNS Tools
+# Install DNS Scanning Tools
 echo "Installing DNS Tools..."
 # Install Whois
 echo "Installing Whois..."
@@ -23,9 +23,39 @@ sudo apt install whois -y
 echo "Installing nslookup, dig, host dns tools..."
 sudo apt install dnsutils -y
 
+# Install fierce
+echo "Installing fierce..."
+sudo apt install fierce -y
+
+# Install dnsrecon
+echo "Installing dnsrecon..."
+sudo apt install dnsrecon -y
+
+# Install dnsenum
+echo "Installing dnsenum..."
+sudo apt install dnsenum -y
+
+# Install sublist3r
+echo "Installing sublist3r..."
+sudo apt install sublist3r -y
+
+# Web Tools
+# dnsdumpster: https://dnsdumpster.com/
+
 # -----------------------------
 
-# Install Scanning Tools
+# Install Email Scanning Tools
+# # Install theHarvester (kali linux tool)
+# echo "Installing theHarvester..."
+# sudo apt install theharvester -y
+
+# Install Recon-ng
+echo "Installing Recon-ng..."
+sudo apt install recon-ng -y
+
+# -----------------------------
+
+# Install Service / Network Scanning Tools
 # Install Network Scanning Tools
 echo "Installing Network Scanning Tools..."
 # Install Nmap
@@ -38,6 +68,15 @@ echo "Installing RustScan..."
 wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
 sudo dpkg -i rustscan_2.0.1_amd64.deb
 rm ./rustscan_2.0.1_amd64.deb
+
+# Install nikto
+echo "Installing nikto..."
+sudo apt install nikto -y
+
+# Install WPscan: a special tool for wordpress
+echo "Installing WPscan..."
+sudo apt install ruby-full -y
+sudo gem install wpscan
 
 # -----------------------------
 
@@ -72,10 +111,25 @@ rm ./msfinstall
 
 # -----------------------------
 
+# Install Hash Cracking Tools
+echo "Installing Hash Cracking Tools..."
+# Install hashcat
+echo "Installing hashcat..."
+sudo apt install hashcat -y
+
+# Install hashid
+echo "Installing hashid..."
+sudo apt install hashid -y
+
+# # Install hash-identifier (kali linux tool)
+# echo "Installing hash-identifier..."
+# sudo apt install hash-identifier -y
+
+# -----------------------------
+
 # # Install Vulnerability Scanning Tools
 # echo "Installing Vulnerability Scanning Tools..."
-# # Install OpenVAS
-# # Installation Steps is from https://greenbone.github.io/docs/latest/22.4/source-build/index.html
-# echo "Installing OpenVAS..."
-# sudo apt install openvas -y
+# # Install GVM
+# echo "Installing GVM..."
+# sudo apt install gvm -y
 # sudo gvm-setup
